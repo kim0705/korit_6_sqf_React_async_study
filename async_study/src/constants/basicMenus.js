cef18879dd4a0fe48e40c30a27c502664b14a737
kim0705 <dbwls138@naver.com>
2024-07-25 17:43:26 +0900

    등록, 조회

--- async_study/src/constants/basicMenus.js
+++ async_study/src/constants/basicMenus.js
@@ -1,5 +1,7 @@
 import { IoCreateOutline, IoRemoveCircleOutline, IoSearchOutline } from "react-icons/io5";
 import { CiEdit } from "react-icons/ci";
+import { RiComputerLine } from "react-icons/ri";
+
 export const BASIC_MENU = [
     {
         id: 1,
@@ -36,5 +38,23 @@ export const BASIC_MENU = [
         name: "Promise학습",
         path: "/async/basic/promise",
         icon: <IoCreateOutline />
+    },
+    {
+        id: 7,
+        name: "사이즈등록",
+        path: "/async/basic/size/register",
+        icon: <IoCreateOutline />
+    },
+    {
+        id: 8,
+        name: "색상등록",
+        path: "/async/basic/color/register",
+        icon: <IoCreateOutline />
+    },
+    {
+        id: 9,
+        name: "컴퓨터",
+        path: "/computer",
+        icon: <RiComputerLine />
     }
 ];
